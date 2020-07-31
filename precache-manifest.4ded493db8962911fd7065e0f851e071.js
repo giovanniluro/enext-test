02b61fb83f9bd0abe717406203b4585c2659480f
Giovanni Luro <giovanni.luro@outlook.com>
2020-07-30 21:58:41 -0300

    Updates

---
+++ precache-manifest.4ded493db8962911fd7065e0f851e071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d07a46a35c8e26815fe40b5a9798b2",
+    "url": "/enext-test/index.html"
+  },
+  {
+    "revision": "e6289cba268c3400704d",
+    "url": "/enext-test/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "e6289cba268c3400704d",
+    "url": "/enext-test/static/js/2.447c5acd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/enext-test/static/js/2.447c5acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b84bd109eaca3ce21e",
+    "url": "/enext-test/static/js/main.95ca7d0a.chunk.js"
+  },
+  {
+    "revision": "1795a44326d82f8d9e5c",
+    "url": "/enext-test/static/js/runtime-main.04f4706e.js"
+  }
+]);
